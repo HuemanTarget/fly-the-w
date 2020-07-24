4cef934a104e9701a8c3b36c93119ee4dd8e7ccb
PompousPro <pompouspro@PompousPros-MacBook-Pro.local>
2020-07-24 15:17:37 -0700

    add background and button

--- App.js
+++ App.js
@@ -1,12 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <TouchableOpacity
+        style={{
+          borderWidth: 2,
+          borderColor: "#CC3433",
+          backgroundColor: "#CC3433",
+          marginTop: 10,
+          width: 200,
+          height: 40,
+          alignItems: "center",
+          justifyContent: "center",
+          zIndex: 1000,
+          elevation: 1000,
+        }}
+      >
+        <Text
+          style={{
+            color: "white",
+            fontSize: 30,
+            fontWeight: "bold",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          Fly The W
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -14,8 +40,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#0E3386",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
