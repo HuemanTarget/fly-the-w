3d2b39e8f73298f1d47bcd664e7612e9ce3b1c46
PompousPro <pompouspro@PompousPros-MacBook-Pro.local>
2020-07-25 14:13:33 -0700

    add play and stop button for music

--- App.js
+++ App.js
@@ -3,16 +3,22 @@ import { StyleSheet, Text, View, TouchableOpacity, Image } from "react-native";
 import { Audio } from "expo-av";
 import Facebook from "./components/Facebook";
 import Fireworks from "react-native-fireworks";
+import { FontAwesome } from "@expo/vector-icons";
+
+const soundObject = new Audio.Sound();
 
 export default function App() {
-  const [flag, setFlag] = useState(require("./assets/w_flag.png"));
+  const [flag, setFlag] = useState(null);
   const [textValueOne, setTextValueOne] = useState("");
   const [textValueTwo, setTextValueTwo] = useState("");
   const [textValueThree, setTextValueThree] = useState("");
   const [facebook, setFacebook] = useState(null);
   const [fireworks, setFireworks] = useState(null);
-
-  const soundObject = new Audio.Sound();
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [icon, setIcon] = useState(
+    <FontAwesome name="play" size={30} color="white" />
+  );
 
   // const audio = async () => {
   //   try {
@@ -23,6 +29,61 @@ export default function App() {
   //     console.log("sound didnt load");
   //   }
   // };
+  const playAudio = async () => {
+    if (isLoaded == true) {
+      if (isPlaying == true) {
+        await soundObject.stopAsync();
+        setIsPlaying(false);
+        setIcon(<FontAwesome name="play" size={30} color="white" />);
+      } else {
+        await soundObject.playAsync();
+        setIsPlaying(true);
+        setIcon(<FontAwesome name="stop" size={30} color="white" />);
+      }
+    } else {
+      try {
+        await soundObject.loadAsync(require("./assets/goodman.mp3"));
+        await soundObject
+          .playAsync()
+          .then(async (playbackStatus) => {
+            // console.log(playbackStatus);
+            setIcon(<FontAwesome name="stop" size={30} color="white" />);
+            setIsPlaying(true);
+            setIsLoaded(true);
+            setTimeout(() => {
+              soundObject.unloadAsync();
+              setIsLoaded(false);
+              setIcon(<FontAwesome name="play" size={30} color="white" />);
+            }, playbackStatus.playableDurationMillis);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+  // const stopFlag = () => {
+  //   setFlag(require("./assets/w_flag.png"));
+  // };
+  // const playAudio = async () => {
+  //   const { sound } = await Audio.Sound.createAsync(
+  //     require("./assets/goodman.mp3"),
+  //     {
+  //       shouldPlay: true,
+  //       isLooping: false,
+  //     }
+  //   );
+  //   const music = sound;
+  //   setPlayingStatus("playing");
+  // };
+
+  // const stopAudio = async () => {
+  //   await music.stopAsync();
+  //   setPlayingStatus("nosound");
+  // };
 
   const handlePress = async () => {
     setFlag(require("./assets/w_flag.gif"));
@@ -31,18 +92,41 @@ export default function App() {
     setTextValueThree("GO!");
     setFacebook(<Facebook />);
     setFireworks(<Fireworks />);
+    playAudio();
 
-    try {
-      const { sound: soundObject, status } = await Audio.Sound.createAsync(
-        require("./assets/goodman.mp3"),
-        {
-          shouldPlay: true,
-        }
-      );
-      console.log("your sound is playing");
-    } catch (error) {
-      console.log("sound didnt play");
-    }
+    // try {
+    //   const { sound } = await Audio.Sound.createAsync(
+    //     require("./assets/goodman.mp3"),
+    //     {
+    //       shouldPlay: true,
+    //       isLooping: false,
+    //     }
+    //   );
+    //   console.log("your sound is playing");
+    // } catch (error) {
+    //   console.log("sound didnt play");
+    // }
+  };
+
+  const turnOff = async () => {
+    setFlag(null);
+    setTextValueOne("");
+    setTextValueTwo("");
+    setTextValueThree("");
+    setFacebook(null);
+    setFireworks(null);
+    // stopAudio();
+    // try {
+    //   const { sound: soundObject, status } = await Audio.Sound.stopAsync(
+    //     require("./assets/goodman.mp3"),
+    //     {
+    //       shouldPlay: false,
+    //     }
+    //   );
+    //   console.log("your sound is not playing");
+    // } catch (error) {
+    //   console.log("sound still playing");
+    // }
   };
 
   return (
@@ -110,6 +194,12 @@ export default function App() {
         </Text>
       </TouchableOpacity>
       {facebook}
+      {/* <TouchableOpacity onPress={() => turnOff()}>
+        <Text style={{ color: "white", paddingTop: 150 }}>Reset Animation</Text>
+      </TouchableOpacity> */}
+      <TouchableOpacity onPress={() => playAudio()} style={{ marginTop: 20 }}>
+        {icon}
+      </TouchableOpacity>
     </View>
   );
 }
